d55cd35c3eef02eb8104b86dcd7418b02a64b586
Jean Galant Yombouno <jeangalanty@gmail.com>
2024-08-01 20:43:35 +0300

    building the image downloader

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -11,7 +16,11 @@ const BillboardClient = () => {
           title="Billboards (0)"
           description="Manage billboards for your store"
         ></Heading>
-        <Button>
+        <Button
+          onClick={() => {
+            router.push(`/${params.storeId}/billboards/new`);
+          }}
+        >
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
