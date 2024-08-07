681a8d064d1540568575266d613ba5d7abb06a78
Jean Galant Yombouno <jeangalanty@gmail.com>
2024-08-07 15:14:31 +0300

    sizes done

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { columns, SizeColumn } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
+        ></Heading>
+        <Button
+          onClick={() => {
+            router.push(`/${params.storeId}/sizes/new`);
+          }}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator></Separator>
+      <DataTable columns={columns} searchKey="name" data={data}></DataTable>
+      <Heading title="API" description="API calls for sizes" />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
+
+export default SizesClient;
